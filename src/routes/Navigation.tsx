f38cbb04d7433f7521f64f2536f78a7e15999d88
juanCamilo <juanmu@bextsa.com>
2022-08-29 19:39:40 -0500

    Style: Descentralized import/export components

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,5 @@
 import {BrowserRouter, NavLink, Routes, Route, Navigate} from "react-router-dom";
-
+import {LazyPage1, LazyPage2, LazyPage3} from "../01-lazyload/pages";
 import logo from '../logo.svg';
 
 export const Navigation = () => {
@@ -11,23 +11,38 @@ export const Navigation = () => {
 
                     <ul>
                         <li>
-                            <NavLink to="home/" className={({isActive}) => isActive ? 'nav-active' : ''}>Home</NavLink>
+                            <NavLink
+                                to="/lazy1"
+                                className={({isActive}) => isActive ? 'nav-active' : ''}
+                            >
+                                Lazy1
+                            </NavLink>
                         </li>
                         <li>
-                            <NavLink to="/about" className={({isActive}) => isActive ? 'nav-active' : ''}>About</NavLink>
+                            <NavLink
+                                to="/lazy2"
+                                className={({isActive}) => isActive ? 'nav-active' : ''}
+                            >
+                                Lazy2
+                            </NavLink>
                         </li>
                         <li>
-                            <NavLink to="/users" className={({isActive}) => isActive ? 'nav-active' : ''}>Users</NavLink>
+                            <NavLink
+                                to="/lazy3"
+                                className={({isActive}) => isActive ? 'nav-active' : ''}
+                            >
+                                Lazy3
+                            </NavLink>
                         </li>
                     </ul>
                 </nav>
                 <Routes>
-                    <Route path="about" element={<h1>About</h1>}/>
-                    <Route path="users" element={<h1>Users</h1>}/>
-                    <Route path="/home" element={<h1>Home</h1>}/>
+                    <Route path="lazy1" element={<LazyPage1 />}/>
+                    <Route path="lazy2" element={<LazyPage2 />}/>
+                    <Route path="lazy3" element={<LazyPage3 />}/>
 
                     {/*Rutas no validas*/}
-                    <Route path={"/*"} element={<Navigate to="/home" replace/>} />
+                    <Route path={"/*"} element={<Navigate to="/lazy1" replace/>} />
                 </Routes>
             </div>
         </BrowserRouter>
