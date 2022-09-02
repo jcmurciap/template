e8be09a7ef27ad3ffe4e92e5bac13e060c5656c6
juanCamilo <juanmu@bextsa.com>
2022-09-02 09:44:41 -0500

    Chore: Receive props from child component

--- src/02-component-patterns/src/components/ProductCard.tsx
+++ src/02-component-patterns/src/components/ProductCard.tsx
@@ -1,15 +1,25 @@
 import styles from '../styles/styles.module.css'
 import {useProduct} from "../hooks/useProduct";
+import noImage from '../assets/no-image.jpg'
 
-export const ProductCard = () => {
+interface Props {
+    product: Product
+}
+
+interface Product {
+    id: string;
+    title: string;
+    img?: string;
+}
+
+export const ProductCard = ({product}:Props) => {
 
     const {counter, handleCounter} = useProduct();
 
     return (
         <div className={styles.productCard}>
-            <img className={styles.productImg} src="./coffee-mug.png" alt="coffe-mug"/>
-
-            <span className={styles.productDescription}>Coffe Mug</span>
+            <img className={styles.productImg} src={product.img ? product.img : noImage} alt="coffe-mug"/>
+            <span className={styles.productDescription}>{product.title}</span>
             <div className={styles.buttonsContainer}>
                 <button className={styles.buttonMinus} onClick={() => handleCounter(-1)}> - </button>
                 <div className={styles.countLabel}>{counter}</div>
--- src/02-component-patterns/src/pages/ShoppingPage.tsx
+++ src/02-component-patterns/src/pages/ShoppingPage.tsx
@@ -1,11 +1,25 @@
 import {ProductCard} from "../components/ProductCard";
 
+const product = {
+    id: "1",
+    title: "Mug",
+    img: './coffee-mug.png'
+}
+
 export const ShoppingPage = () => {
     return (
         <div>
             <h1>Shopping Store</h1>
             <br/>
-            <ProductCard />
+            <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap'
+            }}>
+                <ProductCard product={product}/>
+                <ProductCard product={product}/>
+                <ProductCard product={product}/>
+            </div>
         </div>
     )
 }
