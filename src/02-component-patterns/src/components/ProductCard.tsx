328ca7f71b979b75ebd066ac25974acd2efd1764
juanCamilo <juanmu@bextsa.com>
2022-09-01 22:23:52 -0500

    Chore: Added my first component.

--- src/02-component-patterns/src/components/ProductCard.tsx
+++ src/02-component-patterns/src/components/ProductCard.tsx
@@ -1,8 +1,20 @@
+import styles from '../styles/styles.module.css'
+import {useProduct} from "../hooks/useProduct";
 
 export const ProductCard = () => {
+
+    const {counter, handleCounter} = useProduct();
+
     return (
-        <>
-            <h1>Shopping Card</h1>
-        </>
+        <div className={styles.productCard}>
+            <img className={styles.productImg} src="./coffee-mug.png" alt="coffe-mug"/>
+
+            <span className={styles.productDescription}>Coffe Mug</span>
+            <div className={styles.buttonsContainer}>
+                <button className={styles.buttonMinus} onClick={() => handleCounter(-1)}> - </button>
+                <div className={styles.countLabel}>{counter}</div>
+                <button className={styles.buttonAdd} onClick={() => handleCounter(+1)}> + </button>
+            </div>
+        </div>
     )
 }
--- src/02-component-patterns/src/pages/ShoppingPage.tsx
+++ src/02-component-patterns/src/pages/ShoppingPage.tsx
@@ -2,10 +2,10 @@ import {ProductCard} from "../components/ProductCard";
 
 export const ShoppingPage = () => {
     return (
-        <>
+        <div>
             <h1>Shopping Store</h1>
             <br/>
             <ProductCard />
-        </>
+        </div>
     )
 }
