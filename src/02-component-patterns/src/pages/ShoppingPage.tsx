665b3b6d4d2b7b3f84cbbde4b3026d63ffff2cde
juanCamilo <juanmu@bextsa.com>
2022-09-07 11:34:20 -0500

    Refactor commit #db6248

--- src/02-component-patterns/src/pages/ShoppingPage.tsx
+++ src/02-component-patterns/src/pages/ShoppingPage.tsx
@@ -1,59 +1,12 @@
 import {ProductButtons, ProductCard, ProductImage, ProductTitle} from "../components";
 import '../styles/custom-styles.css'
-import {Product} from "../interfaces/interfaces";
-import {useState} from "react";
-import {debug} from "util";
-
-const product = {
-    id: "1",
-    title: "Mugman",
-    img: './coffee-mug.png',
-}
-const product2 = {
-    id: "2",
-    title: "Coffe Meme",
-    img: './coffee-mug2.png',
-}
-
-const products:Product[] = [product, product2];
-
-interface ProductInCart extends Product {
-    count: number;
-}
+import {useShoppingCart} from "../hooks/useShoppingCart";
+import {products} from "../data/products";
 
 export const ShoppingPage = () => {
 
-//Objeto que viene con muchas llaves de tipo string que a su vez son de tipo ProductInCart
-const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({});
+    const {onProductCountChange, shoppingCart} = useShoppingCart();
 
-const onProductCountChange = ({count, product}:{count:number, product: Product}) => {
-    setShoppingCart(oldShoppingCart => {
-        const productInCart:ProductInCart = oldShoppingCart[product.id] || {...product, count: 0};
-
-        if (Math.max(productInCart.count + count, 0) > 0) {
-            productInCart.count += count;
-
-            return {
-                ...oldShoppingCart,
-                [product.id]: productInCart
-            }
-        }
-
-        //Borrar el producto
-        const {[product.id]:toDelete, ...rest} = oldShoppingCart;
-
-        return rest
-
-        // if (count === 0) {
-        //     const {[product.id]:toDelete, ...rest} = oldShoppingCart
-        //     return rest
-        // }
-        // return {
-        //     ...oldShoppingCart,
-        //     [product.id]: {...product, count}
-        // }
-    })
-}
     return (
         <div>
             <h1>Shopping Store</h1>
